c05217dd88a64e01d1156389df4f60e1b4faba3b
Sander2022164 <2022164@student.cct.ie>
2023-10-24 10:51:38 +0100

    Looping the data
    Looping the read of the data and adding into the array list

--- src/minihw3/MiniHW3.java
+++ src/minihw3/MiniHW3.java
@@ -7,6 +7,7 @@ package minihw3;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +21,21 @@ public class MiniHW3 {
      * @param args the command line arguments
      */
     //Exception added in case file not found
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
         
         //First read the data from the File and storing into an array List
         BufferedReader br = new BufferedReader(new FileReader ("MOCK_DATA.CVS"));
         List<String> allData = new ArrayList<>();
+        
+        String line = br.readLine();//Read the first line of the file
+        
+        //Now reading each line and adding them into the list
+        while(line != null){//looping the process until there is no data/line
+            allData.add(line);//add the line read to the list
+            line = br.readLine();//go forward and read the next line
+        }
+        
         
         
         
