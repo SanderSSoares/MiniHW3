8f52fe50b0a45602c614265bfd2de58544d7a50e
Sander2022164 <2022164@student.cct.ie>
2023-10-24 11:22:37 +0100

    Final comments and debuggs
    File extension was wrong and also some variable declared. Problems were solved and program running

--- src/minihw3/MiniHW3.java
+++ src/minihw3/MiniHW3.java
@@ -27,8 +27,9 @@ public class MiniHW3 {
         // TODO code application logic here
         
         //First read the data from the File and storing into an array List
-        BufferedReader br = new BufferedReader(new FileReader ("MOCK_DATA.CVS"));
         List<String> allData = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader ("MOCK_DATA.csv"));
+        
         
         String line = br.readLine();//Read the first line of the file
         
@@ -38,25 +39,40 @@ public class MiniHW3 {
             line = br.readLine();//go forward and read the next line
         }
         
+        br.close();//Stopping read of file
+        
         //using Shuffle method in order to get random data from the array
         Collections.shuffle(allData, new Random());
         
         int teamSize = 5; //number of people in each team
-        int TotalTeams = 20; //as I have 100 data, in the end 20 teams will be created
+        int totalTeams = 20; //as I have 100 data, in the end 20 teams will be created
         
         List<List<String>> teams = new ArrayList<>(); //Creating a new array list to stored the data
         
         //Looping the process
-        for (int i=0;i<allData.size();i++){
+        for (int i=0;i<totalTeams;i++){
             List<String> singleTeam = new ArrayList<>(); //creating array for a single team
-            for (int k=0; k<teamSize; k++){
-                String person = allData.remove(0);
-                singleTeam.add(person);
+            for (int k=0; k<teamSize; k++){//Loop to add members to the single team
+                if (allData.isEmpty()){
+                    break;// When the data has finished
+                }
+                String person = allData.remove(0);//to remove and add a person to the team
+                singleTeam.add(person);//adding the person to the current team
             }
-            teams.add(singleTeam);
+            teams.add(singleTeam);//now adding the single team to the list of teams.
         }
         
+        //Now printing the teams
+        //Looping the process
         
+        for(int i=0; i<teams.size();i++){
+            
+            System.out.println("Team " + (i+1) + ":");//Printinh the team number. Team 1, Team 2, so on
+            for (String person : teams.get(i)){//looping through the members of the singleTeam
+                System.out.println(person);//Printing the name/line of the data
+            }
+            System.out.println("---------------------------------------------------------");//just to add something between the teams
+        }
     }
     
 }
